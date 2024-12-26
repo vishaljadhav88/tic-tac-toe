225160ae62b203c57e86584a84af4e2b19f9651f
YOUR NAME <YOUR EMAIL@SAMLECOM>
2024-12-26 20:19:43 +0530

    first commit

---
+++ .history/src/App_20241125125944.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import tictoe from './component/tictoe';
+
+function App() {
+  return (
+    <div className="App">
+
+     <h1>hello</h1>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Tictoe from './component/Tictoe';
+function App() {
+  return (
+    <div className="App">
+
+   <Tictoe />
+      </div>
+  );
+}
+
+export default App;
